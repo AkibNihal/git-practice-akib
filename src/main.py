9380001a149210c93c5a6c75d9bcfde3e9cd30d3
Akib Nihal <akibnehal@gmail.com>
2026-05-01 23:49:36 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 import datetime
+from utils import add, subtract
 
 print("Name: Akib Mahmud Nihal")
 print("Date:", datetime.date.today())
+
+print("Addition:", add(6, 3))
+print("Subtraction:", subtract(6, 3))
