62c256ed057e956d38bb1770de8ea91997d2fe89
Akib Nihal <akibnehal@gmail.com>
2026-05-01 23:56:16 +0600

    Add multiply function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import datetime
-from utils import add, subtract
+from utils import add, subtract, multiply
 
 print("Name: Akib Mahmud Nihal")
 print("Date:", datetime.date.today())
 
 print("Addition:", add(6, 3))
 print("Subtraction:", subtract(6, 3))
+print("Multiplication:", multiply(6, 3))
